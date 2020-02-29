2bcdaf8694810564ef42c04ebfa62172d7d767e2
dbr <dbr.onix@gmail.com>
2020-02-29 12:24:56 +1030

    Remove old placeholder test

--- src/tests.rs
+++
@@ -1,8 +0,0 @@
-use crate::YoutubeQuery;
-
-#[test]
-fn test_channel_info() {
-    let yt = YoutubeQuery::new("roosterteeth");
-    let info = yt.info();
-    let cd = info.contentDetails.unwrap();
-}
